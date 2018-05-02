2d9762409090a2badf013d27451190007a91ef11
mrbln <belen.emre@gmail.com>
2018-05-02 13:03:02 +0300

    Handle change on Search Bar
    Adds state to and handles changes on SearchBar

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,14 +1,36 @@
 import React from "react";
 
 export class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ""
+    };
+
+    this.handleOnChange = this.handleOnChange.bind(this);
+  }
+
+  handleOnChange(e) {
+    this.setState({
+      text: e.target.value
+    });
+  }
+
   render() {
     return (
       <div>
         <label>
           Name:
-          <input type="text" name="name" />
+          <input
+            type="text"
+            name="SearchBar"
+            value={this.state.text}
+            onChange={this.handleOnChange}
+          />
         </label>
         <input type="submit" value="Submit" />
+        <h1>Here is your search term:</h1>
+        <h3>{this.state.text}</h3>
       </div>
     );
   }
